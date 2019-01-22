6e382ac51848a04d6dfc475572250fb386c32c81
Gabriel <utilateurmacbookpro@MacBook-Pro-de-Monsieur.local>
2019-01-22 11:52:28 +0100

    moving the card depending on the cursor position

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 const card = document.getElementById('card');
-let mousePosX = 0;
-let mousePosY = 0;
 let active = false;
+let cardPosX = card.offsetLeft;
+let cardPosY = card.offsetTop;
+let mouseClickPosX = 0;
+let mouseClickPosY = 0;
 
 function clickCard(e){
+  /* Get the space between the mouse and the edges of the card*/
+  mouseClickPosX = e.clientX - cardPosX;
+  mouseClickPosY = e.clientY - cardPosY;
+
   active = true;
 }
 
 function moveCard(e){
   if(active){
-    
-    mousePosX = e.clientX;
-    mousePosY = e.clientY;
-    card.style.top = `${mousePosY}px`;
-    card.style.left = `${mousePosX}px`;
+    let mousePosX = e.clientX;
+    let mousePosY = e.clientY;
 
+    card.style.left = `${mousePosX - mouseClickPosX}px`;
+    card.style.top = `${mousePosY - mouseClickPosY}px`;
   }
 }
 
 function dragEnd(){
+  cardPosX = card.offsetLeft;
+  cardPosY = card.offsetTop;
+
   active = false;
 }
 
