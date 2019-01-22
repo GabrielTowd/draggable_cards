42e0993eb434689ed763a6197d495812d575c5ed
Gabriel <utilateurmacbookpro@MacBook-Pro-de-Monsieur.local>
2019-01-22 11:13:30 +0100

    simple dragging

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const card = document.getElementById('card');
+let mousePosX = 0;
+let mousePosY = 0;
+let active = false;
 
-card.addEventListener('click', function(){
-  card.setAttribute('style', 'top : 40%; left: 40%');
-})
+function clickCard(e){
+  active = true;
+}
+
+function moveCard(e){
+  if(active){
+    
+    mousePosX = e.clientX;
+    mousePosY = e.clientY;
+    card.style.top = `${mousePosY}px`;
+    card.style.left = `${mousePosX}px`;
+
+  }
+}
+
+function dragEnd(){
+  active = false;
+}
+
+card.addEventListener('mousedown', clickCard);
+card.addEventListener('mousemove', moveCard);
+card.addEventListener('mouseup', dragEnd);
